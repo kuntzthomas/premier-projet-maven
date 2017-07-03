a40f5c82d90d92577683d3a04f56a4ed89ca13f6
Thomas Kuntz <Thomas.kuntz68@gmail.com>
2017-07-03 17:09:52 +0200

    Add profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,21 @@
 package dev;
+
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( FigletFont.convertOneLine("App"));
-    }
+public class App {
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+
+		// Affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("environnement : " + environnement);
+	}
 }
