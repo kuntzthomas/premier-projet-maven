29ad08816055fe850f91ac1ad82da10e0593b071
Thomas Kuntz <Thomas.kuntz68@gmail.com>
2017-07-03 14:36:00 +0200

    transform to asciiArt

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( FigletFont.convertOneLine("hello World !"));
+        System.out.println( FigletFont.convertOneLine("App"));
     }
 }
